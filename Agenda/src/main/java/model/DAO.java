a6edbbecf21baec536cb4abc910d6e3acefd8488
Alana Daniele de Oliveira Carneiro <97416824+a-natureza@users.noreply.github.com>
2024-10-05 11:01:07 -0300

    Update DAO.java
    eliminando comentarios

--- Agenda/src/main/java/model/DAO.java
+++ Agenda/src/main/java/model/DAO.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 public class DAO {
 	
 	/**  Módulo de conexão *. */
-	// Parâmetros de conexão
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	
 	/** The url. */
@@ -31,7 +30,6 @@ public class DAO {
 	 *
 	 * @return the connection
 	 */
-	// Método de conexão
 	private Connection conectar() {
 		Connection con = null;
 		try {
@@ -52,18 +50,13 @@ public class DAO {
 	public void inserirContato(JavaBeans contato) {
 		String create = "insert into contatos (nome, fone, email, obs) values (?,?,?,?)";
 		try {
-			// abrir a conexão
 			Connection con = conectar();
-			// preparar a query para execução no banco de dados
 			PreparedStatement pst = con.prepareStatement(create);
-			// substituir os parâmetros (?) pelo conteúdo das variáveis JavaBeans
 			pst.setString(1, contato.getNome());
 			pst.setString(2, contato.getFone());
 			pst.setString(3, contato.getEmail());
 			pst.setString(4, contato.getObs());
-			// executar a query
 			pst.executeUpdate();
-			// encerrar a conexão com o banco
 			con.close();
 		} catch (Exception e) {
 			System.out.println(e);
@@ -76,7 +69,6 @@ public class DAO {
 	 * @return the array list
 	 */
 	public ArrayList<JavaBeans> listarContatos() {
-		// criando um objeto para acessa a classe JavaBeans
 		ArrayList<JavaBeans> contatos = new ArrayList<>();
 		String read = "select * from contatos order by nome";
 		try {
@@ -84,13 +76,11 @@ public class DAO {
 			PreparedStatement pst = con.prepareStatement(read);
 			ResultSet rs = pst.executeQuery();
 			while (((ResultSet) rs).next()) {
-				// variáveis de apoio que recebem os dados do banco
 				String id = rs.getString(1);
 				String nome = rs.getString(2);
 				String fone = rs.getString(3);
 				String email = rs.getString(4);
 				String obs = rs.getString(5);
-				// populando o ArrayList
 				contatos.add(new JavaBeans(id, nome, fone, email, obs));
 			}
 			con.close();
@@ -106,7 +96,6 @@ public class DAO {
 	 *
 	 * @param contato the contato
 	 */
-	// selecionar o contato
 	public void selecionarContato(JavaBeans contato) {
 		String read2 = "select * from contatos where id = ?";
 		try {
@@ -115,7 +104,6 @@ public class DAO {
 			pst.setString(1, contato.getId());
 			ResultSet rs = pst.executeQuery();
 			while (((ResultSet) rs).next()) {
-				// setar as variáveis JavaBeans
 				contato.setId(rs.getString(1));
 				contato.setNome(rs.getString(2));
                 contato.setFone(rs.getString(3));
@@ -133,7 +121,6 @@ public class DAO {
 	 *
 	 * @param contato the contato
 	 */
-	// editar o contato
 	public void alterarContato(JavaBeans contato) {
 		String update = "update contatos set nome=?, fone=?, email=?, obs=? where id=?";
 		try {
